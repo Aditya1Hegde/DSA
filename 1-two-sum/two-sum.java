21ed1e6b7421e3449e42f884098d0b04bb76996f
Aditya1Hegde <146844820+Aditya1Hegde@users.noreply.github.com>
2025-07-05 11:53:25 +0530

    Time: 47 ms (13.20%) | Memory: 45.1 MB (30.71%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,28 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        return findPair(nums, target, 0);
-    }
-
-    private int[] findPair(int[] nums, int target, int i) {
-        if (i >= nums.length) {
-            return new int[]{-1, -1}; 
-        }
-        
-        int[] result = findInner(nums, target, i, 0);
-        if (result[0] != -1) {
-            return result; 
-        }
-        return findPair(nums, target, i + 1); 
-    }
-
-
-    private int[] findInner(int[] nums, int target, int i, int j) {
-        if (j >= nums.length) {
-            return new int[]{-1, -1};
-        }
-        if (i != j && nums[i] + nums[j] == target) {
-            return new int[]{i, j}; 
-        }
-        return findInner(nums, target, i, j + 1); 
+        int[] arr={-1,-1};
+      for(int i=0;i<nums.length;i++){
+          for(int j=i+1;j<nums.length;j++){
+                  if(target==nums[i]+nums[j]){
+                      arr[0]=i;
+                      arr[1]=j;
+                      return arr;
+                  
+          }
+      }
+}
+return arr;
     }
 }
